6fbbbbd93c4f81d13d0d1e2f1762767b791ba0a1
Ezio <814711679@qq.com>
2018-04-04 21:21:20 +0800

    fix

--- src/pages/topic/movie/index.vue
+++ src/pages/topic/movie/index.vue
@@ -18,13 +18,35 @@
 
         data () {
             return {
+                data:{},
                 html: '<div>我是HTML代码</div>'
             }
         },
         created () {
-            //http://v3.wufazhuce.com:8000/api/movie/htmlcontent/1273
-        }
 
+        },
+        onLoad: function (options) {
+            // 生命周期函数--监听页面加载
+            //const content_id = options.content_id;
+            const item_id = 1273;
+            console.log("onLoad", item_id);
+            this.getHtmlContent(item_id)
+        },
+        methods: {
+            getHtmlContent: function (item_id) {
+                const url = 'http://v3.wufazhuce.com:8000/api/movie/htmlcontent/' + item_id;
+
+                request.get(url).then(data =>{
+
+                    this.data = data.data;
+//                    var h = data.data.html_content.toString().replaceAll("<!doctype html>","");
+//                    console.log("h", h);
+//                    this.html = "";
+                   // this.html = '<div class="one-title-box">我觉得我爱上了你，也觉得现在应该是春天了</div>'
+                });
+            }
+
+        }
     }
 </script>
 
