ffd076a0dd38528077d99af28dc46e085e078951
Ezio <814711679@qq.com>
2018-04-05 21:28:17 +0800

    首页...

---
+++ src/components/plate.vue
@@ -0,0 +1,40 @@
+<template xmlns:v-bind="http://www.w3.org/1999/xhtml">
+  <div class="content">
+    <img v-bind:src="plate.img_url" class="plate-img"/>
+    <div class="plate-pic">{{plate.title}}|{{plate.pic_info}}</div>
+    <div class="plate-forward">{{plate.forward}}</div>
+    <div class="plate-pic">{{plate.words_info}}</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['plate']
+}
+</script>
+
+<style>
+  .content{
+    background-color: white;
+    margin-bottom: 16px;
+  }
+  .plate-img{
+    width: 100%;
+  }
+  .plate-pic{
+
+    font-size: 12px;
+    text-align: center;
+    line-height: 20px;
+    padding: 20px 0;
+    color: rgba(0,0,0,0.6);
+  }
+  .plate-forward{
+    line-height: 20px;
+    padding: 0 15px;
+    font-size: 16px;
+  }
+
+
+</style>
--- src/pages/one/index.vue
+++ src/pages/one/index.vue
@@ -1,47 +1,94 @@
-<template>
-    <div>
-        <loading_loading></loading_loading>
-        <loading >数据加载中</loading>
-        <ul class="container log-list">
-            <li v-for="(log, index) in logs" :class="{ red: aa }" :key="index" class="log-item">
-                <card :text="(index + 1) + ' . ' + log"></card>
-            </li>
-        </ul>
+<template xmlns:v-on="http://www.w3.org/1999/xhtml">
+    <div class="content">
+        <loading v-show="isLoading">数据加载中</loading>
+        <div v-show="!isLoading">
+            <!--还缺个头 日期 天气-->
+
+            <!--插画-->
+            <plate :plate="plate"></plate>
+            <!--折叠面板-->
+            <div style="margin: 20px 0;">
+                <div class="panel-head" v-on:click="panelShow = !panelShow">独唱团 {{plate.volume}}
+                </div>
+                <!--<div class="panel-head" v-show="panelShow"> 展开</div>-->
+                <div class="panel-content" v-if="panelShow" v-for=" content in contentList">
+                    <p>{{content.title}}</p>
+                </div>
+
+            </div>
+
+        </div>
+
     </div>
 </template>
 
 <script>
-    import {formatTime} from '@/utils/index'
-    import card from '@/components/card'
-    import loading_loading  from '@/components/loading_loading'
+
+    import plate from '@/components/plate'
+
+    import request from '@/utils/request'
+
 
     export default {
         components: {
-            card,
-            loading_loading
+            plate
         },
 
         data () {
             return {
-                logs: []
+                isLoading: true,
+                panelShow: false,
+                weather: {},
+                contentList: [],
+                plate: {},
+                menu: {}
             }
         },
+        onLoad: function (options) {
+            // 生命周期函数--监听页面加载
+            this.getData();
+        },
+        methods: {
+            getData: function () {
+                let url = "http://v3.wufazhuce.com:8000/api/channel/one/0/%E4%B8%8A%E6%B5%B7%E5%B8%82";
+                request.get(url).then(data => {
+                    this.weather = data.data.weather;
+                    this.menu = data.data.menu;
+                    let list = data.data.content_list;
+                    console.log("content_list", list)
+                    for (let i = 0; i < list.length; i++) {
+                        if (i === 0) {
+                            this.plate = list[0];
+                        } else {
+                            this.contentList.push(list[i])
+                        }
+                    }
 
-        created () {
-            const logs = (wx.getStorageSync('logs') || [])
-            this.logs = logs.map(log => formatTime(new Date(log)))
+                    this.isLoading = false;
+                });
+
+            },
         }
     }
 </script>
 
 <style>
-    .log-list {
-        display: flex;
-        flex-direction: column;
-        padding: 40 rpx;
+    .content {
+        background-color: #eeeeee;
     }
 
-    .log-item {
-        margin: 10 rpx;
+    .panel-head {
+        background-color: aquamarine;
+        font-size: 12px;
+        text-align: center;
+        line-height: 20px;
+        padding: 10px 0;
+    }
+
+    .panel-content {
+
+        font-size: 16px;
+        line-height: 20px;
+        padding: 12px 0;
     }
 </style>
